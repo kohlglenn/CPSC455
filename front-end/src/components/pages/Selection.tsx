a8d97b166a3f0c6232b953fcba67eb682b0d5e07
Kohl Peterson <kohlglenn@gmail.com>
2022-06-10 23:05:51 -0700

    added a card for the restaurant on selection page. (#5)

--- front-end/src/components/pages/Selection.tsx
+++ front-end/src/components/pages/Selection.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import LayoutWithAppbar from '../layout/LayoutWithAppbar'
+import sampleData from '../../SampleData.json';
+import {useSelector, useDispatch} from 'react-redux';
+import { ReduxState } from '../../reducers';
+import { GoogleNearbyPlaceResponse, Restaurant } from '../../models';
+import { setRestaurants } from '../../actions';
+import RestaurantCard, { Props } from '../widgets/RestaurantCard';
 
 function Selection() {
+  const restaurants = useSelector((state: ReduxState) => state.restaurants);
+  const [index, setIndex] = React.useState(0);
+  const dispatch = useDispatch();
+  
+  const incrementIndex = () => {
+    setIndex((index + 1) % restaurants.length);
+  }
+  
+  useEffect(() => {
+    if (restaurants.length === 0) {
+      const restaurants = sampleData.results.map((result: GoogleNearbyPlaceResponse) => {
+        const restaurant: Restaurant = {
+          name: result.name,
+          open_now: result.opening_hours.open_now,
+          photos: ["https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"],
+          price_level: result.price_level,
+          rating: result.rating,
+          user_ratings_total: result.user_ratings_total,
+          location: result.geometry.location
+        };
+        return restaurant;
+      });
+      dispatch(setRestaurants(restaurants));
+    }
+  }, []);
+
   return (
     <LayoutWithAppbar>
-        Selection
+        {restaurants.map(r => {
+          const rProps: Props = {
+            img: r.photos[0],
+            imgHeight: 400,
+            name: r.name,
+            rating: r.rating,
+            numRating: r.user_ratings_total,
+            setIndex: incrementIndex
+          };
+          return (
+            <RestaurantCard {...rProps} />
+          );
+        })[index]}
     </LayoutWithAppbar>
   );
 }
--- front-end/src/models/index.ts
+++ front-end/src/models/index.ts
@@ -1,9 +1,29 @@
 import { Action as ReduxAction } from "redux";
 
+export type Action = ReduxAction & { payload: any };
+
 export interface User {
   name: string;
   email: string;
   profileUrl?: string;
 }
 
-export type Action = ReduxAction & { payload: any };
+export interface Restaurant {
+  name: string;
+  open_now: boolean;
+  photos: string[];
+  price_level: number | undefined;
+  rating: number;
+  user_ratings_total: number;
+  location: { lat: number; long: number };
+}
+
+export interface GoogleNearbyPlaceResponse {
+  name: string;
+  geometry: { location: any };
+  opening_hours: { open_now: any };
+  photos: { height: number; width: number; photo_reference: string }[];
+  price_level?: number;
+  rating: number;
+  user_ratings_total: number;
+}
