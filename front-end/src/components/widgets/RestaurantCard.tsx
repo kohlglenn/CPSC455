4e85cf54019efc2c6594a43fef786847d926c5d1
kohlglenn <kohlglenn@gmail.com>
2022-06-11 13:33:03 -0700

    update package.json

--- front-end/src/components/widgets/RestaurantCard.tsx
+++ front-end/src/components/widgets/RestaurantCard.tsx
@@ -5,7 +5,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { RestaurantSharp } from '@mui/icons-material';
 
 export interface Props {
   img: string;
