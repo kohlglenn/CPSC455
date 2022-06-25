99ddc0c8563af7ff0d59279db1c054251c03fceb
kutori <s4x2b@students.cs.ubc.ca>
2022-06-25 09:04:52 -0700

    merge fixes

--- front-end/src/models/rest.ts
+++ front-end/src/models/rest.ts
@@ -1,6 +1,7 @@
 import { ObjectHTMLAttributes } from "react";
 import userCookies from "./userCookies";
 
+//change stuff to use .env instead of hardcode address
 export const getUserAsync = async () => {
     const userid = userCookies.getUser();
     const url = "http://localhost:5000" + '/users/' + userid;
