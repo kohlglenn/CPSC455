2ac886b7a9f9125fd04a7a0e3bbb2b28bf167a57
kohlglenn <kohlglenn@gmail.com>
2022-07-16 11:27:38 -0700

    fix merge conflicts

--- back-end/src/db/mongodb.ts
+++ back-end/src/db/mongodb.ts
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-const queries = require('./queries');
-const generateData = require('./generateData').generateData;
+const mongoose = require("mongoose");
+const queries = require("./queries");
+const generateData = require("./generateData").generateData;
 
-dbstart().catch(err => console.log(err));
+dbstart().catch((err) => console.log(err));
 async function dbstart() {
-    await mongoose.connect('mongodb://localhost:27017/sandbox');
-    //generateData();
+  // await mongoose.connect('mongodb://localhost:27017/sandbox');
+  //generateData();
 }
 
 module.exports = queries;
